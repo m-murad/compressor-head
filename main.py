eb4fcd19166f7d9defd551abd6cc7f9a5b51ff38
m-murad <murad.kuka@gmail.com>
2017-08-19 21:07:15 +0530

    Added basic image compression.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright 2007 Google Inc.
+# Copyright 2017 Murad.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,12 +14,31 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from google.appengine.api import images
+from google.appengine.api import urlfetch
+
 import webapp2
 
-class MainHandler(webapp2.RequestHandler):
+class HomeHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('This is CompressorHead.')
 
+class ImageHandler(webapp2.RequestHandler):
+    def get(self):
+
+        # Getting the URL parameters.
+        image_url = self.request.get('image_url')
+        height = int(self.request.get('height'))
+        width = int(self.request.get('width'))
+
+        image = images.Image(urlfetch.fetch(image_url).content)
+        image.resize(width=width, height=height)
+        output = image.execute_transforms(output_encoding=images.JPEG)
+
+        self.response.headers['Content-Type'] = 'image/jpeg'
+        self.response.write(output)
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', HomeHandler),
+    ('/image/', ImageHandler),
 ], debug=True)
